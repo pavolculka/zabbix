0217df1229280f869329444e5b5c26b40f4b5551
p c <p c>
2017-04-20 21:06:32 +0200

    8

--- zabbix_commands.sh
+++ zabbix_commands.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 wget https://raw.githubusercontent.com/pavolculka/zabbix/master/zbx_template_outlet_servers.xml -O /etc/zabbix/templates/zbx_template_outlet_servers.xml
 
 wget https://raw.githubusercontent.com/pavolculka/zabbix/master/zabbix_import_template.py -O /etc/zabbix/scripts/zabbix_import_template.py
